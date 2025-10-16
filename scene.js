2378c990c5e433076ad423fec3fa72308aba9b59
Rob Davidson <your-email@example.com>
2025-10-16 20:52:29 +0100

    Basic starting point

---
+++ scene.js
@@ -0,0 +1,48 @@
+// Helper function to get CSS root color variables
+function getColor(cssVar) {
+    return getComputedStyle(document.documentElement)
+        .getPropertyValue(cssVar)
+        .trim();
+}
+
+// Basic three.js setup
+const container = document.getElementById('app');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    55, // FOV
+    window.innerWidth / window.innerHeight, // Aspect ratio
+    0.1, // Near plane
+    100, // Far plane
+);
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
+
+// Redraw scene on window resize
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// Renderer setup
+renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio); // Render at full native screen resolution
+renderer.setSize(window.innerWidth, window.innerHeight); // Use the full window size
+renderer.setClearColor(getColor('--gray-900')); // Set background color to gray-900
+container.appendChild(renderer.domElement);
+
+// Camera controls
+const controls = new THREE.OrbitControls(camera, renderer.domElement); // Enable orbit controls
+controls.enableDamping = true; // Enable damping for smoother movement
+controls.maxDistance = Math.sqrt(30 * 30 + 30 * 30 + 30 * 30); // Limit max camera distance to (30,30,30)
+
+// Scene
+scene.add(createAxes());
+
+// Animation loop
+function animate() {
+    controls.update(); // smooth camera movement
+    renderer.render(scene, camera); // draw scene from camera
+    requestAnimationFrame(animate); // repeat each frame
+}
+animate();
---
+++ sceneElements/axes.js
@@ -0,0 +1,4 @@
+function createAxes() {
+    const axes = new THREE.AxesHelper(1.5);
+    return axes;
+}
