f362dff19bb56ba8f2798937b250d08c77dbc95d
Rob Davidson <your-email@example.com>
2025-10-17 23:07:22 +0100

    enable anti aliasing and add circle

--- scene.js
+++ scene.js
@@ -25,6 +25,8 @@ renderer.setSize(window.innerWidth, window.innerHeight); // Use the full window 
 renderer.setClearColor(getColor('--gray-900')); // Set background color to gray-900
 container.appendChild(renderer.domElement);
 
+renderer.antialias = true; // Enable antialiasing
+
 // Camera controls
 const controls = new THREE.OrbitControls(camera, renderer.domElement); // Enable orbit controls
 controls.enableDamping = true; // Enable damping for smoother movement
@@ -36,12 +38,15 @@ stars = createStars(params.starCount);
 plane = createPlane(params.showPlane);
 sphere = createSphere(params.showSphere);
 circleCenter = createCircleCenter(params.showCircleCenter);
+circle = createCircle(params.planeHeight);
+
 // Add scene elements to the scene
 scene.add(axes);
 scene.add(stars);
 scene.add(plane);
 scene.add(sphere);
 scene.add(circleCenter);
+scene.add(circle);
 
 // Animation loop
 function animate() {
