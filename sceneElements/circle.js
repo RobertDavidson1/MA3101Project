41be6c9e8bec49b664bb81b6bbf2633534270ce8
Rob Davidson <your-email@example.com>
2025-10-17 23:09:38 +0100

    add a circle cut through sphere by plane -  from maths derived in writeup

--- sceneElements/circle.js
+++ sceneElements/circle.js
@@ -19,3 +19,35 @@ function createCircleCenter(visible) {
     sphere.visible = visible;
     return sphere;
 }
+
+function createCircle(planeHeight) {
+    // r = (1-h^2)^(1/2)
+    const h = Math.max(-1, Math.min(1, planeHeight));
+    const r = Math.sqrt(Math.max(0, 1 - h * h));
+
+    // C = (0,0,h)
+    const C = new THREE.Vector3(0, 0, 1).multiplyScalar(planeHeight);
+
+    const i = new THREE.Vector3(1, 0, 0);
+    const j = new THREE.Vector3(0, 1, 0);
+
+    const segments = 256;
+    const points = [];
+    for (let k = 0; k < segments; k++) {
+        const t = (k / segments) * 2 * Math.PI;
+        const cosT = Math.cos(t);
+        const sinT = Math.sin(t);
+
+        // p(t) = C + r[cos(t)i + sin(t)j]
+        const x = C.x + r * (cosT * i.x + sinT * j.x);
+        const y = C.y + r * (cosT * i.y + sinT * j.y);
+        const z = C.z + r * (cosT * i.z + sinT * j.z);
+        points.push(new THREE.Vector3(x, y, z));
+    }
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const material = new THREE.LineBasicMaterial({
+        color: getColor('--red-500'),
+    });
+    const circle = new THREE.LineLoop(geometry, material);
+    return circle;
+}
