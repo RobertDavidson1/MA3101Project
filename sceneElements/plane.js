f9d637288196393cea73dda557dfedbbe8ac4492
Rob Davidson <your-email@example.com>
2025-10-17 21:29:30 +0100

    function to create a plane

---
+++ sceneElements/plane.js
@@ -0,0 +1,20 @@
+function createPlane() {
+    // Create a plane with a width and height of 1
+    const planeGeometry = new THREE.PlaneGeometry(3, 3);
+
+    // Create a material for the plane
+    const material = new THREE.MeshBasicMaterial({
+        color: getColor('--red-500'),
+        side: THREE.DoubleSide, // Render both sides of the plane
+        transparent: true, // Allow the plane to be transparent
+        opacity: 0.25,
+        depthTest: false,
+    });
+
+    // Take the geometry and material and form a mesh
+    const plane = new THREE.Mesh(planeGeometry, material);
+
+    // Rotate plane so it's in the xy plane
+    plane.rotation.x = Math.PI / 2;
+    return plane;
+}
