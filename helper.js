7c0c617839db454b250196fbc92b7e22cdd4ed7e
Rob Davidson <your-email@example.com>
2025-10-18 13:38:18 +0100

    - move getColor helper to formula

--- helper.js
+++
@@ -1,6 +0,0 @@
-// Helper function to get CSS root color variables
-function getColor(cssVar) {
-    return getComputedStyle(document.documentElement)
-        .getPropertyValue(cssVar)
-        .trim();
-}
