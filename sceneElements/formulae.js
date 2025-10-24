2541d5e797a247eb3e8d4ed19a362be5ebd431b8
Rob Davidson <your-email@example.com>
2025-10-24 17:37:46 +0100

    add createPointOnIntersectionCircle to formulae.js

--- sceneElements/formulae.js
+++ sceneElements/formulae.js
@@ -17,6 +17,32 @@ function calculateRadius(planeHeight) {
 }
 
 function calculateCircleCenter(planeHeight) {
-    // C = (0,0,h)
     return new THREE.Vector3(0, 0, 1).multiplyScalar(planeHeight);
+}
+
+// Creats a point on the circle formed by the intersection of the plane and the sphere
+// planeHeight: The height of the plane
+// t: The angle around the circle
+function createPointOnIntersectionCircle(planeHeight, t) {
+    // i and j hat vectors
+    const i = new THREE.Vector3(1, 0, 0);
+    const j = new THREE.Vector3(0, 1, 0);
+
+    const cos_T = cosT(t);
+    const sin_T = sinT(t);
+
+    // h = planeHeight
+    const h = planeHeight;
+    const r = calculateRadius(h);
+
+    // C = (0,0,h)
+    const C = calculateCircleCenter(planeHeight);
+
+    // p(t) = C + r[cos(t)i + sin(t)j]
+    const x = C.x + r * (cos_T * i.x + sin_T * j.x);
+    const y = C.y + r * (cos_T * i.y + sin_T * j.y);
+    const z = C.z + r * (cos_T * i.z + sin_T * j.z);
+
+    // Return the point as a THREE.Vector3
+    return new THREE.Vector3(x, y, z);
 }
