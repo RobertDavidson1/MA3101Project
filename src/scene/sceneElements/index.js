39fedaf1e7c414812078be44cdd7a79d128bc1a4
Rob Davidson <your-email@example.com>
2025-10-24 23:00:50 +0100

    index to store elements

---
+++ src/scene/sceneElements/index.js
@@ -0,0 +1,15 @@
+// Export all the functions
+export { createAxes } from './axes.js';
+export { createCircle } from './circle.js';
+export { createCircleCenter } from './circleCenter.js';
+export { createPlane } from './plane.js';
+export { create_p_t_point, create_p_t_vector } from './pointOnCircle.js';
+export { createSphere, createSphereCenter } from './sphere.js';
+export { createStars } from './stars.js';
+export {
+    createCosVector,
+    createSinVector,
+    createCosineCompletion,
+    createSinCompletion,
+} from './trigVectors.js';
+export { createSphereNormal, createAcclerationVector } from './vectors.js';
