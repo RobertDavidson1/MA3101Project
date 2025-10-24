bac43e17b20b7dc0a37f42ad67c3f90d333a1eeb
Rob Davidson <your-email@example.com>
2025-10-24 18:04:47 +0100

    change sphere color

--- sceneElements/sphere.js
+++ sceneElements/sphere.js
@@ -7,10 +7,10 @@ function createSphere(visible) {
 
     // Create a material for the sphere
     const sphereMaterial = new THREE.MeshBasicMaterial({
-        color: getColor('--neutral-50'),
+        color: getColor('--nuetral-500'),
         wireframe: true,
         transparent: true,
-        opacity: 0.1,
+        opacity: 0.05,
     });
 
     // Take the geometry and material and form a mesh
