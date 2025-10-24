025efbb04b4a4dd9e49a4dbf84a7fb35949a8fac
Rob Davidson <your-email@example.com>
2025-10-24 22:59:52 +0100

    - Tidy up imports using index.js - move scene setup to scene.js - Remove global variabes

--- src/main.js
+++ src/main.js
@@ -1,58 +1,22 @@
-import { createAxes } from './scene/sceneElements/axes.js';
-import { createCircle } from './scene/sceneElements/circle.js';
-import { createCircleCenter } from './scene/sceneElements/circleCenter.js';
-import { createPlane } from './scene/sceneElements/plane.js';
 import {
+    createAxes,
+    createCircle,
+    createCircleCenter,
+    createPlane,
     create_p_t_point,
     create_p_t_vector,
-} from './scene/sceneElements/pointOnCircle.js';
-import { createSphere } from './scene/sceneElements/sphere.js';
-import { createSphereCenter } from './scene/sceneElements/sphere.js';
-import { createStars } from './scene/sceneElements/stars.js';
-import {
+    createSphere,
+    createSphereCenter,
+    createStars,
     createCosVector,
     createSinVector,
     createCosineCompletion,
     createSinCompletion,
-} from './scene/sceneElements/trigVectors.js';
-import {
     createSphereNormal,
     createAcclerationVector,
-} from './scene/sceneElements/vectors.js';
+} from './scene/sceneElements/index.js';
+
 import { SceneManager } from './core/SceneManager.js';
-
-const container = document.getElementById('app');
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(
-    55, // FOV
-    window.innerWidth / window.innerHeight, // Aspect ratio
-    0.1, // Near plane
-    100, // Far plane
-);
-camera.up.set(0, 0, 1); // make Z the up-axis
-camera.position.set(3, 3, 3);
-
-// Redraw scene on window resize
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
-
-// Renderer setup
-const renderer = new THREE.WebGLRenderer();
-renderer.setPixelRatio(window.devicePixelRatio); // Render at full native screen resolution
-renderer.setSize(window.innerWidth, window.innerHeight); // Use the full window size
-renderer.setClearColor(getColor('--neutral-800')); // Set background color to gray-900
-container.appendChild(renderer.domElement);
-
-renderer.antialias = true; // Enable antialiasing
-
-// Camera controls
-const controls = new THREE.OrbitControls(camera, renderer.domElement); // Enable orbit controls
-controls.enableDamping = true; // Enable damping for smoother movement
-controls.maxDistance = Math.sqrt(30 * 30 + 30 * 30 + 30 * 30); // Limit max camera distance to (30,30,30)
 
 // Make camera and controls globally available
 window.camera = camera;
@@ -111,20 +75,3 @@ function animate() {
     requestAnimationFrame(animate); // repeat each frame
 }
 animate();
-
-// Make all creation functions globally available
-window.createAxes = createAxes;
-window.createCircle = createCircle;
-window.createCircleCenter = createCircleCenter;
-window.createPlane = createPlane;
-window.create_p_t_point = create_p_t_point;
-window.create_p_t_vector = create_p_t_vector;
-window.createSphere = createSphere;
-window.createSphereCenter = createSphereCenter;
-window.createStars = createStars;
-window.createCosVector = createCosVector;
-window.createSinVector = createSinVector;
-window.createCosineCompletion = createCosineCompletion;
-window.createSinCompletion = createSinCompletion;
-window.createSphereNormal = createSphereNormal;
-window.createAcclerationVector = createAcclerationVector;
