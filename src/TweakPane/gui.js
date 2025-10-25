d157d18a3c8e8127b7dfe94058694200063d1a91
Rob Davidson <your-email@example.com>
2025-10-25 12:05:04 +0100

    Add labels to gui

--- src/TweakPane/gui.js
+++ src/TweakPane/gui.js
@@ -1,9 +1,12 @@
 import { p } from './parameters.js';
 import { createVisibilityFolder } from './Folders/Visibility.js';
 import { createControlsFolder } from './Folders/Controls.js';
+import { setLabelsVisible } from '../scene/sceneElements/labels.js';
 
 const pane = new Tweakpane.Pane();
 
 // Create the visibility folder
 const VisibilityFolder = createVisibilityFolder(pane);
 const ControlsFolder = createControlsFolder(pane);
+
+setLabelsVisible(p.showLabels);
