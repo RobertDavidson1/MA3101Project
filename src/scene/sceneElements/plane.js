900a8e6ac576e16ee7da31789f6ef0774c2bc764
Rob Davidson <your-email@example.com>
2025-10-25 12:02:01 +0100

    Add color check for the plane to change to green when it forms a geodesic

--- src/scene/sceneElements/plane.js
+++ src/scene/sceneElements/plane.js
@@ -1,11 +1,12 @@
 // Setting h = z makes rendering the plane cheaper
-export function createPlane(visible) {
+export function createPlane(planeHeight, visible) {
+    const color = getColor(planeHeight === 0 ? '--green-950' : '--neutral-950');
     // Create a plane with a width and height of 1
     const planeGeometry = new THREE.PlaneGeometry(3, 3);
 
     // Create a material for the plane
     const material = new THREE.MeshBasicMaterial({
-        color: getColor('--neutral-900'),
+        color: getColor('--neutral-950'),
         side: THREE.DoubleSide, // Render both sides of the plane
         transparent: true, // Allow the plane to be transparent
         opacity: 0.5,
@@ -14,6 +15,9 @@ export function createPlane(visible) {
 
     // Take the geometry and material and form a mesh
     const plane = new THREE.Mesh(planeGeometry, material);
+
+    // Position the plane at the specified height
+    plane.position.z = planeHeight;
 
     // Set initial visibility
     plane.visible = visible;
